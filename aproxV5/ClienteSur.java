281bffd8bcf779034e494348a35fb383b4274f30
CDCM <cristianzdmc14@gmail.com>
2023-06-27 19:45:30 -0500

    Update ClienteSur.java

--- aproxV5/ClienteSur.java
+++ aproxV5/ClienteSur.java
@@ -1,33 +1,76 @@
-
-/**
- * Write a description of class ClienteSur here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class ClienteSur
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class ClienteSur
-     */
-    public ClienteSur()
-    {
-        // initialise instance variables
-        x = 0;
+public class ClienteSur{
+    private final String identificacion;
+    private String nombre;
+    private String genero;
+    private double calificacionuno;
+    private double calificaciondos;
+    private double calificaciontres;
+    private double calificacioncuatro;
+    
+    public String getIdentificacion(){
+        return identificacion;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        if(nombre != null) this.nombre = nombre;
+        else this.nombre = "";
+    }
+    
+    public String getGenero(){
+        return genero;
+    }
+    public void setGenero(String genero){
+        if(genero != null) this.genero = genero;
+        else this.genero = "";
+    }
+    
+    public double getCalificacionuno(){
+        return calificacionuno;
+    }
+    public void setCalificacionuno(double calificacionuno){
+        if(calificacionuno >= 0) this.calificacionuno = calificacionuno;
+        else this.calificacionuno = 0;
+    }
+    
+    public double getCalificaciondos(){
+        return calificaciondos;
+    }
+    public void setCalificaciondos(double calificaciondos){
+        if(calificaciondos >= 0) this.calificaciondos = calificaciondos;
+        else this.calificaciondos = 0;
+    }
+    
+    public double getCalificaciontres(){
+        return calificaciontres;
+    }
+    public void setCalificaciontres(double calificaciontres){
+        if(calificaciontres >= 0) this.calificaciontres = calificaciontres;
+        else this.calificaciontres = 0;
+    }
+    
+    public double getCalificacioncuatro(){
+        return calificacioncuatro;
+    }
+    public void setCalificacioncuatro(double calificacioncuatro){
+        if(calificacioncuatro >= 0) this.calificacioncuatro = calificacioncuatro;
+        else this.calificacioncuatro = 0;
+    }
+    
+    ClienteSur(String identificacion, String nombre, String genero, double calificacionuno, double calificaciondos, double calificaciontres, double calificacioncuatro){
+        if(identificacion != null) this.identificacion = identificacion;
+        else this.identificacion = "";
+        setNombre(nombre);
+        setGenero(genero);
+        setCalificacionuno(calificacionuno);
+        setCalificaciondos(calificaciondos);
+        setCalificaciontres(calificaciontres);
+        setCalificacioncuatro(calificacioncuatro);        
+    }
+    
+    double calcularPromedio(){
+        return (calificacionuno + calificaciondos + calificaciontres + calificacioncuatro) / 4;
     }
 }
